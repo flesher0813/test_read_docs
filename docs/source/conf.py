97540c137c347ea4517df04b14aee918562711f6
flesher0813 <33923823+flesher0813@users.noreply.github.com>
2025-09-28 10:48:55 +0800

    Add files via upload

---
+++ docs/source/conf.py
@@ -0,0 +1,72 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Unified Cache Manager"
+copyright = "2025, Unified Cache Manager Team"
+author = "Unified Cache Manager Team"
+release = ""
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Copy from https://github.com/vllm-project/vllm/blob/main/docs/source/conf.py
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "myst_parser",
+    "sphinxarg.ext",
+    "sphinx_design",
+    "sphinx_togglebutton",
+    "sphinx_substitution_extensions",
+]
+
+myst_enable_extensions = ["colon_fence", "substitution"]
+
+# templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_title = project
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_css_files = ["css/logo.css"]
+html_theme_options = {
+    "path_to_docs": "docs/source",
+    "repository_url": "https://github.com/ModelEngine-Group/unified-cache-management",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "logo": {
+        "image_light": "logos/UCM-light.png",
+        "image_dark": "logos/UCM-dark.png",
+        "alt_text": "UCM",
+    },
+}
+import os
+import shutil
+
+
+def copy_images(app, exception):
+    if exception is None:
+        # After building，copy docs/source/images to _build/html/images/
+        src = os.path.abspath(os.path.join(app.srcdir, "images"))
+        dst = os.path.join(app.outdir, "images")
+        os.makedirs(dst, exist_ok=True)
+        shutil.copytree(src, dst, dirs_exist_ok=True)
+
+
+def setup(app):
+    app.connect("build-finished", copy_images)
+
+
+# language = 'zh_CN'
---
+++ docs/source/index.md
@@ -0,0 +1,57 @@
+# Welcome to Unified Cache Manager
+
+:::{figure} ./logos/UCM-light.png
+:align: center
+:alt: UCM
+:class: no-scaled-link
+:width: 50%
+:::
+
+:::{raw} html
+<p style="text-align:center">
+<strong>Unified Cache Manager
+</strong>
+</p>
+
+<p style="text-align:center">
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<a class="github-button" href="https://github.com/ModelEngine-Group/unified-cache-management" data-show-count="true" data-size="large" aria-label="Star">Star</a>
+<a class="github-button" href="https://github.com/ModelEngine-Group/unified-cache-management/subscription" data-icon="octicon-eye" data-size="large" aria-label="Watch">Watch</a>
+<a class="github-button" href="https://github.com/ModelEngine-Group/unified-cache-management/fork" data-icon="octicon-repo-forked" data-size="large" aria-label="Fork">Fork</a>
+</p>
+:::
+
+Make KVCache Great Again!
+
+## Documentation
+
+:::{toctree}
+:caption: Getting Started
+:maxdepth: 1
+getting-started/quick_start
+getting-started/installation_gpu
+getting-started/installation_npu
+:::
+
+:::{toctree}
+:caption: User Guide
+:maxdepth: 1
+user-guide/prefix-cache/index
+user-guide/sparse-attention/index
+user-guide/pd-disaggregation/index
+user-guide/engine-integration/index
+:::
+
+:::{toctree}
+:caption: Developer Guide
+:maxdepth: 1
+developer-guide/sparse_attention
+developer-guide/pd_disaggregation
+developer-guide/contribute
+:::
+
+:::{toctree}
+:caption: About Us
+:maxdepth: 1
+about
+:::
